de626de9ed873d8f229fc8f746bc0af01ca60aec
akamatsukouta <kouta0618_5fam2000@icloud.com>
2021-04-21 15:00:27 +0900

    usersコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'cookings#index'
   
-  
+  resources :users, only: [:show]
   resources :cookings, only: [:index, :new, :create, :show, :destroy, :edit, :update] do
     resources :comments, only: [:create]
     collection do
