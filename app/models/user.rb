babe089908e4e68fed8ffcaac65b9fe83dc4df74
akamatsukouta <kouta0618_5fam2000@icloud.com>
2021-04-21 12:08:01 +0900

    commentsコントローラー作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  #has_many :cookings
-  #has_many :comments
+  has_many :cookings
+  has_many :comments
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i
  with_options presence: true do
     validates :nickname
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'cookings#index'
   
+  
   resources :cookings, only: [:index, :new, :create, :show, :destroy, :edit, :update] do
     collection do
       get 'search'
     end
+    resources :comments, only: [:create]
   end
+
 end
