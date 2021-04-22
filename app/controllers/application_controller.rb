83869e9e38392340113f508e116921b442562c90
akamatsukouta <kouta0618_5fam2000@icloud.com>
2021-04-23 03:21:40 +0900

    authenticate_or_request_with_http_basicメソッドをapplication_controller.rbに記述してBasic認証導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth
 
   private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :sex, :cooking_experience] )
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'ak' && password == '777'
+    end
   end
 end
 
