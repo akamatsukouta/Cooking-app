9b4641834ab1878265d8301225f062a0ab6c6df9
akamatsukouta <kouta0618_5fam2000@icloud.com>
2021-04-24 18:50:57 +0900

    ルーティング記述

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   
   resources :users, only: [:show]
   resources :cookings, only: [:index, :new, :create, :show, :destroy, :edit, :update] do
+    resources :likes, only: [:create, :destroy]
     resources :comments, only: [:create]
     collection do
       get 'search'
