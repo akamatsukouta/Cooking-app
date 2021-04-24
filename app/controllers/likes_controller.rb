1f68fa4b1c1201d019a279a7ae0183660fd3d325
akamatsukouta <kouta0618_5fam2000@icloud.com>
2021-04-24 19:09:46 +0900

    likesコントローラにcreateアクションとdestroyアクション記述

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,12 @@
 class LikesController < ApplicationController
+  def create
+    @like = current_user.like.create(cooking_id: params[:cooking_id])
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    @like = Like.find_by(cooking_id: params[:cooking_id], user_id: current_user.id)
+    @like.destroy
+    redirect_back(fallback_location: root_path)
+  end
 end
