ad2ad9f90364ba8d79ae435a992d59ad001835bf
akamatsukouta <kouta0618_5fam2000@icloud.com>
2021-04-30 22:30:06 +0900

    postgresql導入

--- app/controllers/cookings_controller.rb
+++ app/controllers/cookings_controller.rb
@@ -48,7 +48,7 @@ class CookingsController < ApplicationController
   end
 
   def ranking
-    @ranks = Cooking.find(Like.group(:cooking_id).order('count(cooking_id) DESC').limit(3).pluck(:cooking_id))
+    @all_ranks = Cooking.find(Like.group(:cooking_id).order('count(cooking_id) DESC').limit(3).pluck(:cooking_id))
     @cookings = Cooking.order("created_at DESC")
   end
 
